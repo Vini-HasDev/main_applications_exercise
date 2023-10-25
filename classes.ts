d397916f1a7a91adae518cab36842071b2d35823
Ayato <ayato@pop-os.localdomain>
2023-10-25 12:50:55 -0300

    Adicionados novos comentarios sobre a instalação do ts-node

--- classes.ts
+++ classes.ts
@@ -23,3 +23,15 @@ class Tv {
 const Tv1 = new Tv('Samsung', 49, '1920x1080', ['HDMI, DISPLAY PORT, ETHERNET']);
 
 Tv1.turnOn();
+
+/* 
+    Comandos necessários para instalar o ts-node:
+
+npm init -y
+npm i --save-dev typescript@4.4
+npm i -D typescript@4.4
+npx tsc --init
+npm o --save-dev ts-node@10.9.1
+npx ts-node ${nome do arquivo}.ts
+
+    */ 
