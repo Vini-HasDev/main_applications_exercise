3aa943281cdef988cdf2083beb529c7d67d81d87
Ayato <ayato@pop-os.localdomain>
2023-10-28 11:52:55 -0300

    Exercicios de fixação POO

--- classes.ts
+++ classes.ts
@@ -1,28 +1,39 @@
 class Tv {
-    brand: string;
-    size: number;
-    resolution: string;
-    connections: string[];
-    connectedTo?: string;
-    
-    
-    constructor (b: string, s: number, r: string, c: string[]) {
-        console.log(`Creating Tv ${b}`);
-        this.brand = b;
-        this.size = s;
-        this.resolution = r;
-        this.connections = c;
-    };
+    private _brand: string;
+    private _size: number;
+    private _resolution: string;
+    private _connections: string[];
+    private _connectedTo?: string;
+    constructor(brand: string, size: number, resolution: string, connections: string[]) {
+     this._brand = brand;
+      this._size = size;
+     this._resolution = resolution;
+      this._connections = connections;
+    }
 
-    turnOn () {
-        console.log(`The Tv ${this.brand} was tunrned ON, size: ${this.size}", resolution: ${this.resolution} and connections: ${this.connections}`);
-    };
+    turnOn():void {
+        console.log(`TV ${this._brand}, ${this._size}", resolution: ${this._resolution} \navailable connections: ${this._connections}`);
+     }
+      get connectedTo(): string | undefined {
+        return this._connectedTo;
+      }
+   
 
+    set connectedTo(value: string | undefined) {
+        // permite setar undefined ou uma conexão que esteja no `connections`
+        if (!value || this._connections.includes(value)) {
+          this._connectedTo = value;
+          console.log(this._connectedTo);
+        } else {
+          console.log('Sorry, connection unavailable!');
+        }
+      }
 };
 
-const Tv1 = new Tv('Samsung', 49, '1920x1080', ['HDMI, DISPLAY PORT, ETHERNET']);
-
-Tv1.turnOn();
+const tv1 = new Tv('LG', 32, '4K', ['HDMI', 'DISPLAY PORT', 'ETHERNET']);
+ tv1.turnOn();
+tv1.connectedTo = 'HDMI';
+console.log('Connected to: ', tv1.connectedTo);
 
 /* 
     Comandos necessários para instalar o ts-node:
